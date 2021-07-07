0de4a86f9a07d07adb308ee52e328030433d2fe2
Emile <emilefeltesse@gmail.com>
2021-07-07 16:43:49 +0200

    Rien compris mais 1 classe de code

---
+++ MyFirstGUI/src/GUI.java
@@ -0,0 +1,56 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+// D'apres tuto sur YT
+public class GUI implements ActionListener{
+	
+	private int count=0;
+	private JFrame frame;
+	private JPanel panel;
+	private JButton button;
+	private JLabel label;
+
+	public GUI() {
+		
+		frame = new JFrame();
+		button = new JButton("Clic clic!");
+		button.addActionListener(this);
+		label = new JLabel();
+	    panel = new JPanel();
+		
+		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+		panel.add(label);
+		
+		frame.add(panel,BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Notre GUI");
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+		
+	}
+	
+	/** MAIN */
+	public static void main(String[] args) {
+		new GUI();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Nombre de clics: "+ count);	
+		
+	}
+
+}
